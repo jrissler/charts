fbb5d67c19b2d08916da27a4282f18982724d207
James Rissler <risslerjames@gmail.com>
2023-12-28 11:04:58 -0500

    Update deps

--- mix.exs
+++ mix.exs
@@ -30,10 +30,10 @@ defmodule Charts.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.28.5", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.15.0", only: :test}
+      {:credo, "~> 1.7.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4.2", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.31.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18.0", only: :test}
     ]
   end
 end
