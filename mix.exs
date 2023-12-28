3e8ea65a74bb057f9361df31b6e1b9a4606bf34a
James Rissler <risslerjames@gmail.com>
2023-12-28 11:14:08 -0500

    Update elixir / erlang versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Charts.MixProject do
     [
       app: :charts,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
