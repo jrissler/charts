7c26f0543f863c8fa10dfae8de7b7fab9ddca001
James Rissler <risslerjames@gmail.com>
2025-03-17 08:24:10 -0400

    Update deps.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Charts.MixProject do
     [
       app: :charts,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.18.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
@@ -30,10 +30,10 @@ defmodule Charts.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.7.2", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4.2", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.31.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.18.0", only: :test}
+      {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.37.3", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18.5", only: :test}
     ]
   end
 end
